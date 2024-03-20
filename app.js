bd12872c826efce72def48fdce4b1ff765a571f7
Carlos Moreira <ingdevmoreira@gmail.com>
2024-03-19 22:22:23 -0500

    Add mayus validation

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 function encriptar() {
 
     var text = document.getElementById("input-entrada").value;
-    hiddeImage(text);
-    var textCifrado = text.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
-    document.getElementById("input-salida").value = textCifrado;
+    if (validateText(text)) {
+        hiddeImage(text);
+        var textCifrado = text.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+        document.getElementById("input-salida").value = textCifrado;
+    }
 }
 
 function desencriptar() {
     var text = document.getElementById("input-entrada").value;
-    hiddeImage(text);
-    var textCifrado = text.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
-    document.getElementById("input-salida").value = textCifrado;
+    if (validateText(text)) {
+        hiddeImage(text);
+        var textCifrado = text.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+        document.getElementById("input-salida").value = textCifrado;
+    }
 }
 
 function hiddeImage(text) {
@@ -20,5 +24,18 @@ function hiddeImage(text) {
     } else {
         document.getElementById("input-salida").hidden = true;
         document.getElementById("munecoImg").hidden = false;
+    }
+}
+
+function validateText(text) {
+    var regex = /[A-Z]+$/
+    if (regex.test(text)) {
+        document.getElementById("input-salida").hidden = true;
+        document.getElementById("munecoImg").hidden = false;
+        document.getElementById("resultMessage").innerText = "Hay letras mayúsculas o con acentos,o hay simbolos especiales"
+        return false;
+    } else {
+        document.getElementById("resultMessage").innerText = "Ningún mensaje fue encontrado"
+        return true;
     }
 }
